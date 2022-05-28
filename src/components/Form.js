172eceaa5415250a6f77478e3e006957c39f8880
mavililker <ilkermavili5369@hotmail.com>
2022-05-28 15:01:57 +0300

    Init

---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useChat } from "../contexts/ChatContext";
+import { sendMessage } from "../socketApi";
+
+function Form() {
+  const { setChat } = useChat();
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) {
+      return;
+    }
+
+    setChat((prev) => [...prev, { text, isFromMe: true }]);
+    sendMessage(text);
+    setText("");
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="message"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder = "Start typing"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
